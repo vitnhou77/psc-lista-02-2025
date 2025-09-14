35c719ff761b1c19545a7fccd758c18f4c3fa26b
Vitor Hugo Pereira <vitorhugopereirasilva12@gmail.com>
2025-09-14 18:31:50 -0300

    AT02.java

---
+++ AT02.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class AT02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Digite o segundo número: ");
+        int num2 = sc.nextInt();
+
+        int soma = num1 + num2;
+        int subtracao = num1 - num2;
+        int multiplicacao = num1 * num2;
+
+        System.out.println("Soma: " + soma);
+        System.out.println("Subtração: " + subtracao);
+        System.out.println("Multiplicação: " + multiplicacao);
+
+        sc.close();
+    }
+}
